d17848b7daa9fd7011f286fbb40be739e116e199
Chandan Kumar Rout <chandankrout95@gmail.com>
2026-01-17 12:22:21 +0530

    server setup is done .

---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+// Connect DB
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Server running 🚀');
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
